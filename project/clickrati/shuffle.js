457aa99dafc0022458b121d0c3953a406b08962c
ocowchun <ocowchun@gmail.com>
2015-01-05 10:36:19 +0800

    fix sample js

--- project/clickrati/shuffle.js
+++ project/clickrati/shuffle.js
@@ -17,60 +17,99 @@ var sample = function(cb) {
 	var lineCount = 0;
 
 	var lines = [];
-	var fuckNum=40428960;
-	var sampleIndex = getSampleIndex(10000, fuckNum);
+	var fuckNum = 40428960;
+	var sampleIndex = getSampleIndex(40000, fuckNum);
 
+	var currentLines = 0;
+	var errors = [];
+	// console.log(sampleIndex.length);
+	// var currentTarget = sampleIndex.shift();
 	var tr = through(function(line) {
-		// if (lineCount  10) {
-		if (_.contains(sampleIndex, lineCount)) {
+		if (_.indexOf(sampleIndex, lineCount, true) != -1) {
+			// if (true) {
+			// if (lineCount === currentTarget) {
 			line = line.toString();
-			lines.push(line);
+
+			if (valideData(line)) {
+				lines.push(line);
+
+			} else {
+				errors.push({
+					lineCount: lineCount,
+					line: line
+				});
+			}
+			currentLines++;
+			// currentTarget = sampleIndex.shift();
+			if (currentLines % 4000 === 0) {
+				var currentP = currentLines / 4000;
+				// console.log(lines.length);
+				console.log(currentP + "%");
+				append(lines);
+				lines = [];
+			}
+
 		}
+
 		lineCount++;
 	}, function() {
-		cb(lines);
+		console.log("done");
+		if (errors.length > 0) {
+			console.log("errors");
+			console.log(errors);
+		}
+		// cb(lines);
 	});
+
 	instream.pipe(split())
 		.pipe(tr);
 
 }
 
-function getSampleIndex(size, max) {
-	var result = [],
-		i = 0;
+function append(lines) {
+	// lines.con
+	var result = lines.join("\n")+"\n";
+	var fileName = "/Users/ocowchun/Dropbox/nccu/R_Computing _for_Business_Data_Analytics/temp/train.4k.csv";
+	fs.appendFile(fileName, result, function(err) {
+		if (err) throw err;
+		console.log('The "data to append" was appended to file!');
+	});
 
-	while (i < size) {
-		var candidate = _.random(0, max);
-		if (!_.contains(result, candidate)) {
-			result.push(candidate);
-			i++
-		}
-	}
-	return result;
+
+
+	// fs.writeFile(fileName, result, function(err) {
+	// 	if (err) {
+	// 		console.log("ERROR:")
+	// 		console.log(err);
+	// 	} else {
+	// 		console.log("The file was saved!");
+	// 	}
+	// });
+
 }
 
-// console.log(getSampleIndex(10000,40428960));
+function getSampleIndex(size, max) {
+	var store = [];
+	for (var i = 0; i < max; i++) {
+		store.push(i);
+	}
+	console.log("hello");
+	var result = _.shuffle(store).slice(0, size);
 
-// console.log(_.random(0, 1))
+	console.log("sample done");
+	var sortIds = _.sortBy(result, function(num) {
+		return num;
+	});
+	return sortIds;
+}
+
+
+function valideData(line) {
+	return line.split(',').length === 24
+}
 
 sample(function(lines) {
 	writeLines(lines);
 	// console.log(lines.length);
 });
 
-// var a = ["hello", "bar"];
-// console.log(a.join("\n"))
-
-
-var writeLines = function(lines) {
-	// lines.con
-	var result = lines.join("\n");
-	var fileName = "/Users/ocowchun/Dropbox/nccu/R_Computing _for_Business_Data_Analytics/temp/test.csv";
-	fs.writeFile(fileName, result, function(err) {
-		if (err) {
-			console.log(err);
-		} else {
-			console.log("The file was saved!");
-		}
-	});
-}
